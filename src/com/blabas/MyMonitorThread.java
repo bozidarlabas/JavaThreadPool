81f59370698f7292ee96900e1c3c23bc5704df9b
bozidar <bozidar.labas@hotmail.com>
2015-11-05 12:07:26 +0100

    Implemented monitor thread that will print the executor info at certain time interval

--- src/com/blabas/MyMonitorThread.java
+++ src/com/blabas/MyMonitorThread.java
@@ -1,11 +1,50 @@
 package com.blabas;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
- * Created by macbook on 05.11.2015..
+ * Monitor thread that will print the executor information at certain time interval.
  */
 public class MyMonitorThread implements Runnable {
+
+    private ThreadPoolExecutor executor;
+
+    private int seconds;
+
+    private boolean run = true;
+
+    public MyMonitorThread(ThreadPoolExecutor executor, int delay){
+        this.executor = executor;
+        this.seconds = delay;
+    }
+
+    public void shutdown(){
+        this.run = false;
+    }
+
     @Override
     public void run() {
+        while(run){
+            System.out.println(
+                    String.format("" +
+                            "[monitor] [%d%d] Active: %d, " +
+                            "Completed: %d, Task: %d, " +
+                            "isShutdown: %s, " +
+                            "isTerminated: %s",
+                            this.executor.getPoolSize(),
+                            this.executor.getCorePoolSize(),
+                            this.executor.getActiveCount(),
+                            this.executor.getCompletedTaskCount(),
+                            this.executor.getTaskCount(),
+                            this.executor.isShutdown(),
+                            this.executor.isTerminated()));
 
+            try {
+                Thread.sleep(seconds * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
